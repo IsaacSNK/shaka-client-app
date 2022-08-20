b80ab2450e1cbf86f0576666a7fda600468ef09b
Isaac Ramirez Herrera <joseisaacr@microsoft.com>
2022-08-19 22:24:45 -0600

    Scaffolding

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    devtool: 'source-map',
+    optimization: {
+        usedExports: true
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        // disable type checker - we will use it in fork plugin
+                        transpileOnly: true
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: []
+};
